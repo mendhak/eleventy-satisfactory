d82e8dec47fb966078a76c184dd68f9136cf2eaa
mendhak <mendhak@users.noreply.github.com>
2023-01-08 17:32:55 +0000

    Make the headings permalinks

--- .eleventy.js
+++ .eleventy.js
@@ -67,11 +67,7 @@ module.exports = function(eleventyConfig) {
     html: true,
     linkify: true
   }).use(markdownItAnchor, {
-    permalink: markdownItAnchor.permalink.ariaHidden({
-      placement: "after",
-      class: "direct-link",
-      symbol: "#"
-    }),
+    permalink: markdownItAnchor.permalink.headerLink(),
     level: [1,2,3,4],
     slugify: eleventyConfig.getFilter("slugify")
   });
