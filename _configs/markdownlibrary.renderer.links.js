a3bf45fb3765f70632ce2290eb23641c1e26c01e
mendhak <mendhak@users.noreply.github.com>
2023-01-20 18:38:59 +0000

    Use optional chaining operator

--- _configs/markdownlibrary.renderer.links.js
+++ _configs/markdownlibrary.renderer.links.js
@@ -7,7 +7,7 @@ module.exports = function (tokens, idx, options, env, self, pathPrefix) {
 
   let link = tokens[0].attrGet('href');
   if (link && link.endsWith(".md")) {
-    if (env.collections && env.collections.posts && env.collections.posts[0]) {
+    if (env.collections?.posts?.[0]) {
       // Match on the file name without extension, eg 'customary-lorem-ipsum', then use its URL in the rendered link.
       let found = env.collections.posts.find(p =>  path.parse(p.inputPath).name === path.parse(link).name);
       tokens[0].attrSet('href', path.join(pathPrefix, found.url));
