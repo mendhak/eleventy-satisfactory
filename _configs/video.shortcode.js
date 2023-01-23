6a6643f70bcf2100f993d4f1896b4df2ea705bf1
mendhak <mendhak@users.noreply.github.com>
2023-01-23 20:05:16 +0000

    Shortcode for video

---
+++ _configs/video.shortcode.js
@@ -0,0 +1,46 @@
+const path = require("path");
+
+/**
+ * Paired shortcode to display an iframe video. Detects YouTube or Vimeo.
+ */
+
+module.exports = function(url, width) {
+  let style = '';
+  let iframeUrl = '';
+  if(width==="unconstrained"){
+    style="max-width: unset;";
+  }
+
+  let youTubeId = getVideoIdFromYouTubeURL(url);
+  if(youTubeId){
+    iframeUrl = `//www.youtube.com/embed/${youTubeId}`;
+  }
+  else {
+    let vimeoId = getVideoIdFromVimeoURL(url);
+    iframeUrl = `https://player.vimeo.com/video/${vimeoId}`;
+  }
+
+  let videoHtml = `<div class="video" style="${style}">
+    <iframe src="${iframeUrl}" frameborder="0" allowfullscreen></iframe>
+  </div>`;
+
+  return videoHtml;
+
+
+};
+
+
+function getVideoIdFromYouTubeURL(url) {
+	let regExp = /^.*(youtu.be\/|v\/|u\/\w\/|embed\/|watch\?v=|&v=)([^#&?]*).*/;
+	let match = url.match(regExp);
+
+	return match && match[2].length === 11 ? match[2] : null;
+}
+
+function getVideoIdFromVimeoURL(url){
+
+  let regExp = /^https:\/\/vimeo\.com\/([^#&?]*)/;
+	let match = url.match(regExp);
+	return match && match[1] ? match[1] : null;
+
+}
