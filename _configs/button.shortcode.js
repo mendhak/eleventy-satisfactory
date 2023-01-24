53a7225ac3cf56555b93b01f32fa68b30dd0e87a
mendhak <mendhak@users.noreply.github.com>
2023-01-24 06:50:23 +0000

    Button with link

---
+++ _configs/button.shortcode.js
@@ -0,0 +1,10 @@
+module.exports = function(text, url) {
+
+  let buttonHtml = `<button>${text}</button>`;
+  if(url){
+    buttonHtml = `<a href="${url}">${buttonHtml}</a>`;
+  }
+
+  return buttonHtml;
+}
+
