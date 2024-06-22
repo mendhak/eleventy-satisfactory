4fea7908b390be704ed1e5eecd68137dfe667281
mendhak <mendhak@users.noreply.github.com>
2024-06-22 17:41:50 +0100

    Test for Github repo card

---
+++ tests/post-with-github-repo-card.spec.ts
@@ -0,0 +1,23 @@
+import { test, expect, Page } from '@playwright/test';
+
+let siteURL;
+
+test.beforeEach(async ({page, baseURL}) => {
+    await page.goto('github-repo-card/');
+    siteURL = baseURL;
+});
+
+
+test.describe('Post with Github Repo Card', () => {
+
+  test.only('Repo Card is rendered with description', async ({ page }) => {
+
+    await expect(page.getByText('Lightweight GPS Logging Application For Android')).toBeVisible();
+
+    let sendLocator = page.getByText('mozilla/send');
+    const mozillaSendCount = await sendLocator.count();
+    await expect(mozillaSendCount).toBe(2);
+
+  });
+
+});
