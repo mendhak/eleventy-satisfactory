e6e1a3ce9adebf2e9836d89875c5e2e6ddd20602
mendhak <mendhak@users.noreply.github.com>
2025-02-10 20:51:58 +0000

    Test light and dark theme

---
+++ tests/light-dark-theme.spec.ts
@@ -0,0 +1,20 @@
+import { test, expect, Page } from '@playwright/test';
+
+let siteURL;
+
+test.beforeEach(async ({page, baseURL}) => {
+    await page.goto('/');
+    siteURL = baseURL;
+});
+
+
+test.only('Light and Dark Theme', async ({ page }) => {
+
+  let articleContainer = await page.locator('body');
+  await expect(articleContainer).toHaveCSS('background-color', 'rgb(255, 255, 255)');
+  await page.emulateMedia({ colorScheme: 'dark' });
+  await expect(articleContainer).toHaveCSS('background-color', 'rgb(51, 51, 51)');
+  // sleep 5s
+  await page.waitForTimeout(5000);
+
+});
